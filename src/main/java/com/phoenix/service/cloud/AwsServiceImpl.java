40f541a755aa5593a683bfe9ed79044c9c90c648
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-10 20:42:47 +0100

    cloud services

---
+++ src/main/java/com/phoenix/service/cloud/AwsServiceImpl.java
@@ -0,0 +1,11 @@
+package com.phoenix.service.cloud;
+
+import java.io.File;
+import java.util.Map;
+
+public class AwsServiceImpl implements CloudService{
+    @Override
+    public Map<?, ?> upload(File file, Map<?, ?> params) {
+        return null;
+    }
+}
---
+++ src/main/java/com/phoenix/service/cloud/CloudineryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.phoenix.service.cloud;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+@Service("cloudinery")
+public class CloudineryServiceImpl implements CloudService {
+
+    @Autowired
+    Cloudinary cloudinary;
+
+    @Override
+    public Map<?, ?> upload(File file, Map<?, ?> params) throws IOException {
+        return cloudinary.uploader().upload(file, params);
+
+    }
+}
