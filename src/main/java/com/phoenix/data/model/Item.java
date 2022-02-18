f6e80d04b220503bde098dc48ed21b9bc7260f95
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-18 16:16:14 +0100

    modified item

--- src/main/java/com/phoenix/data/model/Item.java
+++ src/main/java/com/phoenix/data/model/Item.java
@@ -9,32 +9,26 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 public class Item {
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @OneToOne
     private Product product;
-
     private Integer quantityAddedToCart;
 
 
-
-
-    public void setQuantityAddedToCart(){
-        if(quantityAddedToCart <= product.getQuantity()){
+    public void setQuantityAddedToCart(){//this validates input before adding to cart at construction
+        if(product.getQuantity() >= quantityAddedToCart){
             this.quantityAddedToCart = quantityAddedToCart;
+        }else {
+            this.quantityAddedToCart = 0;
         }
-
     }
 
     public Item(Product product, int quantityAddedToCart) {
-        if(quantityAddedToCart <= product.getQuantity()){
-            this.quantityAddedToCart = quantityAddedToCart;
 
-        }
+            this.quantityAddedToCart = 0;
+
         this.product = product;
 //        return product;
     }
