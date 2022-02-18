59737fa7f99cf605a7eea05b4dca5dfc08968cdf
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-18 16:15:43 +0100

    modified cart

--- src/main/java/com/phoenix/data/model/Cart.java
+++ src/main/java/com/phoenix/data/model/Cart.java
@@ -13,22 +13,18 @@ import java.util.List;
 @Entity
 //@Builder
 @Data
+
 //@NoArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @CreationTimestamp
     private LocalDateTime createdAt;
-
-    @OneToMany(cascade = CascadeType.PERSIST)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)// fetches the object and its children persist them to db
     private List<Item> itemList;
-
-    @Transient
-    private Double totalPrice;
-
-    public void addItem(Item item){
+    private Double totalPrice;//calculates total price of items inside hte cart
+    public void addItem(Item item){// this creates a new cart list at construction time
         if(itemList == null){
             itemList = new ArrayList<>();
         }
