555531b20cdb3853e761e7a9780815bc859764a0
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-18 16:19:45 +0100

    modified product

--- src/main/java/com/phoenix/data/model/Product.java
+++ src/main/java/com/phoenix/data/model/Product.java
@@ -4,9 +4,7 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Data
