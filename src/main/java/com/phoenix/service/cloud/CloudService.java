868cc57c49e95bacbd5c7b5556fa77e2a7e8666b
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-18 16:21:07 +0100

    cloud services

--- src/main/java/com/phoenix/service/cloud/CloudService.java
+++ src/main/java/com/phoenix/service/cloud/CloudService.java
@@ -1,10 +1,12 @@
 package com.phoenix.service.cloud;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public interface CloudService {
 
-    public Map<?, ?> upload(File file, Map<?,?> params) throws IOException;
+    public Map<?, ?> upload(byte[] bytes, Map<?,?> params) throws IOException;
 }
